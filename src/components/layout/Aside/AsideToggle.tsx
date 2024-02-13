3d82604b0ffa1c5509ae4f9d2b8ba0ae13b8435c
nester-dev <nester.dev@gmail.com>
2024-02-13 09:48:38 +0300

    chore: add data-test-id for AsideToggle

--- src/components/layout/Aside/AsideToggle.tsx
+++ src/components/layout/Aside/AsideToggle.tsx
@@ -3,8 +3,10 @@ import styles from './Aside.module.scss';
 import { MenuFoldOutlined } from '@ant-design/icons';
 import { useAppDispatch, useAppSelector } from '@hooks/typed-react-redux-hooks.ts';
 import { toggleAside } from '@redux/mainStore.ts';
+import useMediaQuery from '@hooks/useMediaQuery.ts';
 
 const AsideToggle: FC = () => {
+    const isMobile = useMediaQuery('(max-width: 480px)');
     const { isAsideCollapsed } = useAppSelector((state) => state.mainState);
     const dispatch = useAppDispatch();
 
@@ -17,7 +19,7 @@ const AsideToggle: FC = () => {
             type='button'
             onClick={handleClick}
             className={styles.toggle}
-            data-test-id='sider-switch'
+            data-test-id={isMobile ? 'sider-switch-mobile' : 'sider-switch'}
         >
             <MenuFoldOutlined />
         </button>
