fa692ae6be42cc37cbbddc744861d826f60c68c6
nester-dev <nester.dev@gmail.com>
2024-02-25 16:06:58 +0300

    feat: implement ability to logout

--- src/components/ui/Menu/Menu.tsx
+++ src/components/ui/Menu/Menu.tsx
@@ -4,13 +4,24 @@ import { menuItems } from '@components/ui/Menu/config.tsx';
 import Logo from '@components/ui/Logo/Logo.tsx';
 
 import styles from './Menu.module.scss';
+import { MenuKeys } from '@components/ui/Menu/types.ts';
+import { MenuInfo } from 'rc-menu/lib/interface';
+import { useLogout } from '@hooks/useLogout.ts';
 
-const Menu: FC = () => (
-    <div className={styles.menu}>
-        <Logo />
+const Menu: FC = () => {
+    const logout = useLogout();
+    const handleClick = ({ key }: MenuInfo) => {
+        if (key === MenuKeys.logout) {
+            logout();
+        }
+    };
 
-        <AntdMenu mode='inline' items={menuItems} />
-    </div>
-);
+    return (
+        <div className={styles.menu}>
+            <Logo />
 
+            <AntdMenu mode='inline' items={menuItems} onClick={handleClick} />
+        </div>
+    );
+};
 export default Menu;
---
+++ src/components/ui/Menu/types.ts
@@ -0,0 +1,7 @@
+export enum MenuKeys {
+    calendar = 'calendar',
+    workouts = 'workouts',
+    achievements = 'achievements',
+    profile = 'profile',
+    logout = 'logout',
+}
